fd2bb2aace724af7eee94277ba8291fe57dfed21
San-Alienware <abwing@abwing.com>
2020-04-21 16:20:20 +0700

    The Battle

---
+++ Assets/Scripts/GateController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GateController : MonoBehaviour
+{
+    public GameObject canvas;
+    public GameObject target;
+    bool triggered = false;
+    bool gateOpen = false;
+    public Animator animator;
+
+    void Update()
+    {
+        if (triggered && Input.GetKeyDown(KeyCode.E))
+        {
+            gateOpen = !gateOpen;
+            animator.SetBool("Open", gateOpen);
+        }
+    }
+    void OnTriggerEnter(Collider collider)
+    {
+        canvas.SetActive(true);
+        triggered = true;
+    }
+    void OnTriggerExit(Collider collider)
+    {
+        canvas.SetActive(false);
+        triggered = false;
+    }
+}
---
+++ Assets/Scripts/TargetMovement.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetMovement : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            if(Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition),out hit, 100))
+            {
+                transform.position = hit.point;
+            }
+        }
+    }
+}
